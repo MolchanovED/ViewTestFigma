6cc356a1744e6bb8f92483de406d75b224b39b4d
Egor Molchanov <93006877+MolchanovED@users.noreply.github.com>
2022-09-16 16:20:39 +0300

    Initial Commit

---
+++ viewTestFigma/viewTestFigmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  viewTestFigmaApp.swift
+//  viewTestFigma
+//
+//  Created by Egor Molchanov on 16.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct viewTestFigmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
